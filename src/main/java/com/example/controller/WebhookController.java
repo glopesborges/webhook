73f0829e4b7281a10b7c832ec7e59e605b1ff85d
Guilherme Borges <g.lopesborges@gresb.com>
2023-07-28 14:54:19 +0200

    Micronaut bug use byte

--- src/main/java/com/example/controller/WebhookController.java
+++ src/main/java/com/example/controller/WebhookController.java
@@ -7,14 +7,16 @@ import io.micronaut.http.annotation.Post;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.Arrays;
+
 @Slf4j
 @Controller("/api")
 @RequiredArgsConstructor
 public class WebhookController {
 
     @Post(uri = "/v1/webhook/callback")
-    public HttpStatus receiveWebhookCallback(@Body String body) {
-        log.info("Received body: {}", body);
+    public HttpStatus receiveWebhookCallback(@Body byte[] body) {
+        log.info("Received body: {}", Arrays.toString(body));
         return HttpStatus.OK;
     }
 
