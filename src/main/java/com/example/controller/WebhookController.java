739ec00bb97bc6e0f8d7a74734a6cad7fb917cfc
Guilherme Borges <g.lopesborges@gresb.com>
2023-07-31 09:49:43 +0200

    Update WebhookController.java

--- src/main/java/com/example/controller/WebhookController.java
+++ src/main/java/com/example/controller/WebhookController.java
@@ -11,6 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 
 import javax.validation.Valid;
 import java.util.Arrays;
+import java.util.Optional;
 
 @Slf4j
 @Validated
@@ -20,9 +21,15 @@ public class WebhookController {
 
 
     @Post(uri = "/v1/webhook/callback", consumes = MediaType.ALL)
-    public HttpStatus receiveWebhookCallback(@Body WebhookRequestBody body) {
-        log.info("Received body: {}", body);
+    public HttpStatus receiveWebhookCallback(@Body Optional<WebhookRequestBody> body) {
+        body.ifPresent(webhookRequestBody -> log.info("Received body: {}", webhookRequestBody));
         return HttpStatus.OK;
     }
+
+//    @Post(uri = "/v1/webhook/callback", consumes = MediaType.ALL)
+//    public HttpStatus receiveWebhook() {
+//        log.info("Received no body");
+//        return HttpStatus.OK;
+//    }
 
 }
